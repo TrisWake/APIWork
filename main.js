e356c49ca79db631771ed50481e6bcf636c3d7f4
Tristan Wakefield <tristan.wakefield@yahoo.com>
2024-12-01 22:31:21 -0600

    complete

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
-const URL = 'https://dog.ceo/api/breeds/image/random'
+const dogAPI = 'https://dog.ceo/api/breeds/image/random'
+const weatherAPI = 'https://goweather.herokuapp.com/weather/'
 const button = document.querySelector('#button')
 button.addEventListener('click', ()=>{
-    fetch(URL)
+    fetch(dogAPI)
     .then((data) => data.json())
     .then((response) => {
-        console.log("Hello")
+        console.log("Random Dog")
        document.querySelector('#image').src = response.message
     })
     .catch((error) => console.log(error));
 })
 
-const Pokemon = 'https://pokeapi.co/api/v2/pokemon/ditto'
-document.querySelector('#button').addEventListener('click', ()=>{
-    const pokeText = document.querySelector('#pokeText').value
-    const pokeDex = `https://goweather.herokuapp.com/weather/${pokeText}`
-    fetch(pokeDex)
-    document.querySelector('#temperature')
-    document.querySelector('#wind')
-    document.querySelector('#description')
-})  
+const pokeText = document.querySelector('#pokeText').value
+const pokeDex = `https://goweather.herokuapp.com/weather/${pokeText}`
+    
+fetch(pokeDex)
+.then((data) => data,json())
+.then((response) => {
+    console.log("Weather Loaded", response)
+    document.querySelector('#temperature').textContent = `Temperatur:${response.temperature}`;
+    document.querySelector('#wind').textContent = `Wind: ${response.wind}`;
+    document.querySelector('#description').textContent = `Description: ${response.description}`
+})
+catch((error) => console.error("Weather Error:", error))
